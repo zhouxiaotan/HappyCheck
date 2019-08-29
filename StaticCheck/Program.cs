5363b16d7976dafc3ebb8e2d9179559c3f105964
zhouxiaotan <zhouxiaotan@gmail.com>
2019-08-29 16:22:04 +0800

    init
    init

---
+++ StaticCheck/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace StaticCheck
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            StaticCheck pack = new StaticCheck();
+            Application.Run(pack);
+        }
+    }
+}
